8638054a3b4c03c51e4d0aa523bcc88dc42b63a2
Даниил Ульский <daniil.ulsky@lanit-tercom.com>
2021-09-30 09:28:08 +0700

    Minor changes in user games

--- frontend/src/app/users/user-tile/user-tile.component.ts
+++ frontend/src/app/users/user-tile/user-tile.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
-import { User } from 'src/app/core/shared/types';
+import { Game, User } from 'src/app/core/shared/types';
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: 'app-user-tile',
@@ -9,12 +10,20 @@ import { User } from 'src/app/core/shared/types';
 export class UserTileComponent implements OnInit {
 
   @Input('user') user: User;
+  public userGames: string = '';
+  public userGamesIsLoading: boolean = true;
 
   constructor(
-    @Inject('rootServerUrl') public rootServerUrl: string
+    @Inject('rootServerUrl') public rootServerUrl: string,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
+    this.userService.getUserGames(this.user.id)
+      .subscribe((games: Game[]) => {
+        this.userGames = games.map((game: Game) => game.name).join(' • ');
+        this.userGamesIsLoading = false;
+      });
   }
 
 }
